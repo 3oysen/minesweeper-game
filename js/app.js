96087510893524153cff3554441266b405f4ad7f
3oysen <103428192+3oysen@users.noreply.github.com>
2023-10-01 20:51:54 +0200

    added classname to the squares

--- js/app.js
+++ js/app.js
@@ -9,11 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
 		const bombsArray = Array(bombAmount).fill("bomb");
 		const emptyArray = Array(width * width - bombAmount).fill("valid");
 		const gameArray = emptyArray.concat(bombsArray);
-		const gameShuffled = gameArray.sort(() => Math.random() - 0.5);
+		const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
 
 		for (let i = 0; i < width * width; i++) {
 			const square = document.createElement("div");
 			square.setAttribute("id", i);
+			square.classList.add(shuffledArray[i]);
 			grid.appendChild(square);
 			squares.push(square);
 		}
