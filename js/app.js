ce5e62582760eba3c94f5685c0f81cd040fd83c1
3oysen <103428192+3oysen@users.noreply.github.com>
2023-10-05 22:11:36 +0200

    added bomb counting logic for neighboring squares in a grid

--- js/app.js
+++ js/app.js
@@ -30,21 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
 			const isRightEdge = i % width === width - 1;
 
 			if (squares[i].classList.contains("valid")) {
-				if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb"))
-					total++;
-				// TO DO!
-				if (
-					i > 9 &&
-					!isRightEdge &&
-					squares[i + 1 - width].classList.contains("bomb")
-				)
-					total++;
-				if (
-					i > 9 &&
-					!isRightEdge &&
-					squares[i + 1 - width].classList.contains("bomb")
-				)
-					total++;
+				if (i > 11 && !isRightEdge && squares[i - 1 - width].classList.contains("bomb")) total++; //NW
+				if (i > 10 && squares[i - width].classList.contains("bomb")) total++; // N
+				if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains("bomb")) total++; // NE
+				if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb")) total++; // E
+				if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains("bomb")) total++; // SE
+				if (i < 89 && squares[i + width].classList.contains("bomb")) total++; // S
+				if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains("bomb")) total++; // SW
+				if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) total++; // W
 
 				squares[i].setAttribute("data", total);
 				console.log(squares[i]);
