179472df34d631bf9f9337de2b816d7f5270540a
3oysen <103428192+3oysen@users.noreply.github.com>
2023-10-06 13:06:04 +0200

    fixed logic. added display of total

--- js/app.js
+++ js/app.js
@@ -31,13 +31,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 			if (squares[i].classList.contains("valid")) {
 				if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) total++; // W
-				if (i > 11 && !isRightEdge && squares[i - 1 - width].classList.contains("bomb")) total++; //NW
-				if (i > 10 && squares[i - width].classList.contains("bomb")) total++; // N
 				if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains("bomb")) total++; // NE
-				if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb")) total++; // E
+				if (i > 10 && squares[i - width].classList.contains("bomb")) total++; // N
+				if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains("bomb")) total++; //NW
 				if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains("bomb")) total++; // SE
 				if (i < 89 && squares[i + width].classList.contains("bomb")) total++; // S
 				if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains("bomb")) total++; // SW
+				if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb")) total++; // E
 
 				squares[i].setAttribute("data", total);
 			}
@@ -54,7 +54,10 @@ document.addEventListener("DOMContentLoaded", () => {
 			let total = square.getAttribute("data");
 			if (total != 0) {
 				square.classList.add("checked");
+				square.innerHTML = total;
+				return;
 			}
+			square.classList.add("checked");
 		}
 	}
 });
